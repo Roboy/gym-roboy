Corrected bug in joint_vel_penalty calculation

gym_roboy/envs/msj_env.py
@@ -34,7 +34,7 @@ class MsjEnv(gym.GoalEnv):
         self._ros_proxy = ros_proxy
         self._set_new_goal()
         some_state = MsjRobotState(
-            joint_angle=self._JOINT_ANGLE_BOUNDS, joint_vel=self._JOINT_VEL_BOUNDS)
+            joint_angle=self._JOINT_ANGLE_BOUNDS, joint_vel=self._GOAL_JOINT_VEL)
         corresponding_worst_state = MsjRobotState(
             joint_angle=-self._JOINT_ANGLE_BOUNDS, joint_vel=-self._JOINT_VEL_BOUNDS)
         self._joint_vel_penalty = joint_vel_penalty
@@ -74,8 +74,7 @@ class MsjEnv(gym.GoalEnv):
     def compute_reward(self, current_state: MsjRobotState, goal_state: MsjRobotState, info=None):
         reward = -_l2_distance(current_state.joint_angle, goal_state.joint_angle)
         if self._joint_vel_penalty:
-            joint_vel_penalty = _l2_distance(current_state.joint_vel, goal_state.joint_vel)
-            reward -= np.exp(joint_vel_penalty)
+            reward = np.linalg.norm(current_state.joint_vel)*(reward - np.exp(reward))
         assert self.reward_range[0] <= reward <= self.reward_range[1], \
             "'{}' not between '{}' and '{}'".format(reward, self.reward_range[0], self.reward_range[1])
         return reward