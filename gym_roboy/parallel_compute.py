9cc5ae96e4af4ed5d765280a4bc8aa57ee29d941
BarisYazici <barisyazici@sabanciuniv.edu>
2019-02-17 05:34:57 -0800

    to pass the test enable single processing. Parallel compute script takes an argument signifying how many cpus will there

---
+++ gym_roboy/parallel_compute.py
@@ -0,0 +1,34 @@
+import os
+import sys
+
+import gym
+import gym_roboy
+from stable_baselines.common.vec_env import DummyVecEnv, SubprocVecEnv
+from stable_baselines import PPO2
+
+arg_workers = int(sys.argv[1])
+
+RESULTS_DIR = "./training_results"
+assert not os.path.isdir(RESULTS_DIR), "Folder '{}' already exists".format(RESULTS_DIR)
+MODEL_FILE = "./model.pkl"
+
+def setup_constructor(multi_process, rank, seed=0):
+
+    def our_env_constructor() -> gym.Env:
+        environment = gym.make("msj-control-v0", multi_process = multi_process, process_idx = rank)
+        environment.seed(seed + rank)
+        return environment
+
+    return our_env_constructor
+
+
+num_cpu = arg_workers
+multi_process = bool(num_cpu > 1)
+env = SubprocVecEnv([setup_constructor(multi_process, i) for i in range(num_cpu)])
+agent = PPO2("MlpPolicy", env, tensorboard_log=RESULTS_DIR)
+
+while True:
+    agent.learn(total_timesteps=1000000)
+    agent.save(MODEL_FILE)
+
+
