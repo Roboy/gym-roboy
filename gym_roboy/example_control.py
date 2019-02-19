70b82a785ecd405c91fc8117f3646c97143b914e
Your Name <you@example.com>
2019-02-19 17:05:49 +0000

    BUGFIX: example_control script was broken

--- gym_roboy/example_control.py
+++ gym_roboy/example_control.py
@@ -3,17 +3,16 @@ import os
 from stable_baselines.common.vec_env import DummyVecEnv
 from stable_baselines import PPO2
 
-from .envs import MsjEnv
+from .envs import MsjEnv, MsjROSBridgeProxy
 
 
 RESULTS_DIR = "./training_results"
-assert not os.path.isdir(RESULTS_DIR), "Folder '{}' already exists".format(RESULTS_DIR)
 MODEL_FILE = "./model.pkl"
 
 
 def main():
     # The algorithms require a vectorized environment to run
-    env_constructor = MsjEnv
+    env_constructor = lambda: MsjEnv(ros_proxy=MsjROSBridgeProxy())
     env = DummyVecEnv([env_constructor])
 
     if os.path.isfile(MODEL_FILE):
